3888e1ca1d03e280e7bdc435a87741822b6aeec7
Nico263-bass <Nicobomolo263@gmail.com>
2026-02-20 00:37:43 +0100

    design et finitions de l'interface

---
+++ projet_panier_e_commerce.js
@@ -0,0 +1,18 @@
+// const test=()=>{
+//     alert("test réussi")
+// };
+// const article=document.getElementsByClassName("gritem");
+//    for (let i = 0; i < article.length; i++) {
+//     let btn=document.createElement('button');
+//     let delBtn=document.createElement('button');
+//     delBtn.textContent="supprimer"
+//     btn.classList.add('btn')
+//     delBtn.classList.add('btn')
+//     btn.textContent="ajouter";
+//     article[i].appendChild(btn);
+//     article[i].appendChild(delBtn);
+
+// }
+    // article.forEach(element => {
+    //     element.addEventListener('click',test)
+    // });
