0d5c027386afd5cd57918229aa09ca3a6aeb25f1
Nico263-bass <Nicobomolo263@gmail.com>
2026-03-06 12:30:15 +0100

    création du panier proprement dit

--- projet_panier_e_commerce.js
+++ projet_panier_e_commerce.js
@@ -1,18 +1,36 @@
-// const test=()=>{
-//     alert("test réussi")
-// };
-// const article=document.getElementsByClassName("gritem");
-//    for (let i = 0; i < article.length; i++) {
-//     let btn=document.createElement('button');
-//     let delBtn=document.createElement('button');
-//     delBtn.textContent="supprimer"
-//     btn.classList.add('btn')
-//     delBtn.classList.add('btn')
-//     btn.textContent="ajouter";
-//     article[i].appendChild(btn);
-//     article[i].appendChild(delBtn);
-
-// }
-    // article.forEach(element => {
-    //     element.addEventListener('click',test)
-    // });
+ /* création d'un panier E-commerce dynamique */
+ const newItem=document.getElementsByClassName('gritem');
+ function insert(){
+     for (let i = 0; i < newItem.length; i++) {
+         newItem[i].classList.add('test')
+     }
+ };
+ insert();
+ const info=document.querySelectorAll('.gritem');
+     const putInfo=()=>{
+      info.forEach(i=>{
+       newInfo=document.createElement('div');
+ /* ajout du style au nouvel élément crée appelé j */
+  const  j=i.appendChild(newInfo);
+      j.classList.add('info')
+      
+    }) };
+ putInfo(); 
+/* Insertion automatique des informations de chaque article*/
+//  pour JS, chaque article a: .gritem >img+.info>(.blocinfo(p-- nomArticle+ span--Comment))+P--prix +.stars avec 5 étoiles
+class artcile {
+    constructor(Id,nom, prix, comment){
+        this.nom = nom;
+        this.prix = prix;
+        this.comment = comment;
+    }
+    getName() {
+        return this.nom;
+    }
+}
+/*création des articles partant de la classe définie ci-haut*/
+const art_1=new artcile(1,'Samsung Galaxie',350.49,'Résistant à l\'eau');
+const art_2=new artcile(2,'Iphone 16',960,'Excellent rapport qualité prix');
+const art_3=new artcile(3,'Iphone 17 pro',1380.89,'le meilleur smartphone du marché en terme d\'autonomie et de qualité photo')
+const art_4=new artcile(4,'Tecno camon 9',190,'Meilleur choix pour moins cher')
+const panier=[art_1,art_2,art_3,art_4];
